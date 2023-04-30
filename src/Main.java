03c57a6cf11f4fed0eef6a68327200b3e47f83f0
Диас <lbrjyz05@mail.ru>
2023-04-30 22:53:03 +0600

    8

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
         /*
-        MyArrayList arrayList = new MyArrayList();
+        MyArrayListStack arrayList = new MyArrayList();
         arrayList.add(5);
         arrayList.add(10);
         arrayList.add(15);
         arrayList.add(20);
         arrayList.add(25);
-        System.out.println(arrayList.getElement(3));
-        arrayList.deleteElement(3);
-        System.out.println(arrayList.getElement(3));
+        System.out.println(arrayList.get(3));
+        arrayList.remove(3);
+        System.out.println(arrayList.get(3));
 
         arrayList.clear();
         if(arrayList.isEmpty()){
@@ -18,7 +19,7 @@ public class Main {
         else{
             System.out.println("not");
             }
-              */
+
         MyLinkedList lists = new MyLinkedList();
         lists.add(15);
         lists.add(575);
@@ -30,5 +31,6 @@ public class Main {
         System.out.println(lists.isSortable());
         lists.sort();
         lists.printList();
+         */
     }
 }
--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,11 +1,21 @@
 import java.util.EmptyStackException;
-public class MyArrayListStack<T> extends MyArrayList<T> {
+public class MyArrayListStack<T> implements Stack<T> {
     MyArrayList<T> list = new MyArrayList<T>();
     private T[] arr;
     private int size;
     public MyArrayListStack(){
         this.arr = (T[]) new Object[5];
         this.size = 0;
+    }
+    public void print_stack(){
+        System.out.print("[");
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i));
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
     public int size() {
         return list.size();

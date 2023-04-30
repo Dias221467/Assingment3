9c4311587287978db979dbc30e3274a4481bb172
Диас <lbrjyz05@mail.ru>
2023-04-30 23:47:52 +0600

    13

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -7,4 +7,17 @@ public class MyArrayListQueue<T> implements Queue<T> {
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+    public void print_queue(){
+        System.out.print("[");
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i));
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+    public void enqueue(T element) {
+        list.add(element);
+    }
 }

a4c2262b31092590e38db1b18aa8f5781d60fefd
Диас <lbrjyz05@mail.ru>
2023-04-30 23:33:49 +0600

    11

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,2 +1,6 @@
-public class MyArrayListQueue {
+import java.util.NoSuchElementException;
+public class MyArrayListQueue<T> implements Queue<T> {
+    MyArrayList<T> list = new MyArrayList<T>();
+    private T[] arr;
+    private int size;
 }

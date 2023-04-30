7b35efc29797f98926d571abadae7a9eff7aa1d0
Диас <lbrjyz05@mail.ru>
2023-04-30 23:34:38 +0600

    12

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -3,4 +3,8 @@ public class MyArrayListQueue<T> implements Queue<T> {
     MyArrayList<T> list = new MyArrayList<T>();
     private T[] arr;
     private int size;
+    public MyArrayListQueue(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
 }

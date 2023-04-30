735de735b25a45c82f3e4235482841b8b64ea18d
Диас <lbrjyz05@mail.ru>
2023-04-30 22:03:11 +0600

    3

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,8 +1,14 @@
+import java.util.EmptyStackException;
 public class MyArrayListStack<T> extends MyArrayList<T> {
+    MyArrayList list = new MyArrayList();
     private T[] arr;
     private int size;
     public MyArrayListStack(){
         this.arr = (T[]) new Object[5];
         this.size = 0;
     }
+    public void push(T element) {
+        list.add(element);
+    }
+
 }
